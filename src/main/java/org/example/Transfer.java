8a5d1358d43bde81b0b816c2b90ea68e08700ab5
Maksim <40239712+mpozdeev@users.noreply.github.com>
2026-03-11 23:36:53 +0400

    update
    добавлен метод printInfoByTransaction()

--- src/main/java/org/example/Transfer.java
+++ src/main/java/org/example/Transfer.java
@@ -18,12 +18,18 @@ public class Transfer {
         if (from.getCurrency().equals(currency) && to.getCurrency().equals(currency)) {
             from.withdraw(amount);
             to.deposit(amount);
+            printInfoByTransaction();
         } else {
-            throw new IllegalArgumentException("Валюты не совпадают:" +
-                    "Валюта для перевода: " + currency +
-                    " from("+from.getCurrency() + ") ->" +
+            throw new IllegalArgumentException("Валюты не совпадают! Валюта для перевода " + currency +
+                    ": from("+from.getCurrency() + ") ->" +
                     " to("+ to.getCurrency() +")");
 
         }
+    }
+
+    private void printInfoByTransaction() {
+        System.out.printf("From: %s\t  To: %s\t amount: %s\t%n",
+                from.getId().toString().substring(0, 8),
+                to.getId().toString().substring(0, 8), amount);
     }
 }
