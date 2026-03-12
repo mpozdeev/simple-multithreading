65ba3efe2aa424619bf81be8fa8e8925df938834
Maksim <40239712+mpozdeev@users.noreply.github.com>
2026-03-12 14:51:55 +0400

    refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,33 +10,37 @@ import java.util.UUID;
 public class Main {
 
     static void main() {
-
         Logger logger = LoggerFactory.getLogger(Main.class);
 
-//        int countTransactions, int countThreads, int countAccounts, Logger logger
-        TransferSimulation transferSimulation =
-                new TransferSimulation(40, 10, 10, logger);
-        transferSimulation.executeTransferInLoop();
+//        executeFirst(logger);
+        executeSecond(logger);
+    }
 
-//        Account account1 = new Account(UUID.randomUUID(), 4000, Currency.RUB);
-//        Account account2 = new Account(UUID.randomUUID(), 6000, Currency.RUB);
-//
-//        double totalBalanceBefore = account1.getBalance() + account2.getBalance();
-//        System.out.println("Total system balance before: " + totalBalanceBefore);
-//
-//        Transfer transfer1 = new Transfer(account1, account2, 200, Currency.RUB);
-//        Transfer transfer2 = new Transfer(account2, account1, 300, Currency.RUB);
-//
-//        List<Transfer> transfers = Arrays.asList(transfer1, transfer2);
-//
-//        TransferSimulation simultaneousTransfers = new TransferSimulation(transfers, logger);
-//        simultaneousTransfers.executeTransfers();
-//
-//        System.out.println("Account 1 balance: " + account1.getBalance());
-//        System.out.println("Account 2 balance: " + account2.getBalance());
-//        double totalBalanceAfter = account1.getBalance() + account2.getBalance();
-//        System.out.println("Total system balance after: " + totalBalanceAfter);
+    private static void executeSecond(Logger logger) {
+//        TransferSimulation transferSimulation = new TransferSimulation(40, 10, 10, logger);
+        TransferSimulation simulation = new TransferSimulation(10, 3, 2, logger);
+        simulation.executeTransferInLoop();
+    }
 
+    private static void executeFirst(Logger logger) {
+        Account account1 = new Account(UUID.randomUUID(), 4000, Currency.RUB);
+        Account account2 = new Account(UUID.randomUUID(), 6000, Currency.RUB);
+
+        double totalBalanceBefore = account1.getBalance() + account2.getBalance();
+        System.out.println("Total system balance before: " + totalBalanceBefore);
+
+        Transfer transfer1 = new Transfer(account1, account2, 200, Currency.RUB);
+        Transfer transfer2 = new Transfer(account2, account1, 300, Currency.RUB);
+
+        List<Transfer> transfers = Arrays.asList(transfer1, transfer2);
+
+        TransferSimulation simultaneousTransfers = new TransferSimulation(transfers, logger);
+        simultaneousTransfers.executeTransfers();
+
+        System.out.println("Account 1 balance: " + account1.getBalance());
+        System.out.println("Account 2 balance: " + account2.getBalance());
+        double totalBalanceAfter = account1.getBalance() + account2.getBalance();
+        System.out.println("Total system balance after: " + totalBalanceAfter);
     }
 }
 
